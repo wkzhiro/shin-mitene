f7cbbc2106fe324ad55712d39fcce7daecc7db21
wkzhiro <wkzhiro0116@yahoo.co.jp>
2025-01-25 23:33:00 +0900

    silk追加

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,6 +9,7 @@ from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 from mitene.views import LikePostView, UnlikePostView, BookmarkView
 
+
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
@@ -26,7 +27,7 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("", include(wagtail_urls)),
+    path("mitene/", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
